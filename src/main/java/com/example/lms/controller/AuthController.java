f35b4b7ff2a9226702ca8652b8e068814376a779
Shirsendu Pramanik <pramanikshirsendu07@gmail.com>
2026-04-19 01:47:27 +0530

    Update AuthController.java

--- src/main/java/com/example/lms/controller/AuthController.java
+++ src/main/java/com/example/lms/controller/AuthController.java
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequiredArgsConstructor
 public class AuthController {
 
